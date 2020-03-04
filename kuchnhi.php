55dbca5f35d42abdcea4f11d469d360587b0398d
monikalongian <57859039+monikalongian@users.noreply.github.com>
2020-03-04 20:04:07 +0530

    yo

--- kuchnhi.php
+++ kuchnhi.php
@@ -1,5 +1,6 @@
 <?php 
 
+$rcno = "4512";
 
 $servername = "localhost";
 $username   = "root";
@@ -24,9 +25,59 @@ if ($conn->connect_error)
      $pid  = $row['pid'];
      $pdate= $row['pdate'];
      $pcity = $row['pcity'];
+}
+}
+
+
+
+ $sql= ("SELECT * FROM insurance WHERE rcno='$rcno' ");
+      $res = $conn->query($sql);
+
+  if ($res->num_rows >0)
+  {
+  while ($row = $res->fetch_assoc()) 
+  {   
+     $rcno = $row['rcno'];
+     $ino  = $row['ino'];
+     $icompany= $row['icompany'];
+     $icity = $row['icity'];
+     $idate=$row['idate'];
+
+}
+}
+
+
+
+ $sql= ("SELECT * FROM vehicle WHERE rcno='$rcno'");
+      $res = $conn->query($sql);
+
+  if ($res->num_rows >0){
+  while ($row = $res->fetch_assoc()) {   
+     $rcno = $row['rcno'];
+     $vno = $row['vno'];
+     $vchassis = $row['vchassis'];
+     $vengine= $row['vengine'];
+     $name = $row['name'];
+     $fname  = $row['fname'];
+     $address= $row['address'];
+     $email = $row['email'];
+     $vcompany = $row['vcompany'];
+     $vtype = $row['vtype'];
+     $vdate = $row['vdate'];
      
-  
+     
+     
 }
 }
+	echo $pid;
+	echo "<br>";
+	echo $pdate;
+	echo "<br>";
+	echo $icompany;
+	echo "<br>";
+	echo $vtype;
+
+
+
 
 ?>
